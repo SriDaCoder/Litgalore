015edd803d9d84548f23dc393a3983a574e18974
SriDaCoder <srinihasakkineni2010@gmail.com>
2024-10-09 17:57:51 +0400

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrWXcVUqkgJ-PfC5a69Tbah1uYELiiy3w",
+  authDomain: "litgalore.firebaseapp.com",
+  databaseurel: "https://litgalore-default-rtdb.firebaseio.com/",
+  projectId: "litgalore",
+  storageBucket: "litgalore.appspot.com",
+  messagingSenderId: "321985840616",
+  appId: "1:321985840616:web:25900a7fcbf61e255c505d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
